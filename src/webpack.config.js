b543bd9c3a69dca132889d66b5a87066440aca50
tomfjug <tomas.fjugstaderiksen@gmail.com>
2019-05-20 16:11:41 +0200

    resources and files

---
+++ src/webpack.config.js
@@ -0,0 +1,22 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  //entry: ["babel-polyfill", "dist/main.js"],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ],	
+},
+  plugins: [htmlPlugin]
+};
